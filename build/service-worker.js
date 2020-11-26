a22d55ca06f61798e9aa98fa26bdd49b2a7c24a9
jaybenaim <benaimjacob@gmail.com>
2020-11-26 12:09:11 -0500

    changed homepage'

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/componentLibrary/precache-manifest.805db54a1e86e85ef9f69b82510c5217.js"
+  "/componentLibrary/precache-manifest.cffeeef3446ebb53c82ceae059d39633.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/componentLibrary/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
