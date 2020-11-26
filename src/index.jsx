0a594eed460bce9b118bfdebc9f09c6a665c2a51
jaybenaim <benaimjacob@gmail.com>
2020-11-26 13:38:01 -0500

    still calling backend

--- src/index.jsx
+++ src/index.jsx
@@ -44,4 +44,4 @@ ReactDOM.render(
  * Be aware that the website will only update to the latest version on the 2nd page visit if it as already cached
  * Learn more about service workers in React: https://create-react-app.dev/docs/making-a-progressive-web-app
  */
-unregister();
+// unregister();
