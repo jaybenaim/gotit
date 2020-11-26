daac5617712d49e22dc6e9aa77c75726ce7c8a49
jaybenaim <benaimjacob@gmail.com>
2020-11-26 18:58:45 -0500

    minor changes to stop build from deploying

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, "build")));
+// app.use(express.static(path.resolve(__dirname, "build")));
 
 
 // Routes
