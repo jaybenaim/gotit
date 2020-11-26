bb14c4b58208a386e235ca53418bed0b4b0d03da
jaybenaim <benaimjacob@gmail.com>
2020-11-26 12:35:45 -0500

    build

---
+++ build/precache-manifest.c209230d70ab6f3e601795468af624b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c2948da4ceab96b5ad0a90fbe0a034",
+    "url": "/gotit/index.html"
+  },
+  {
+    "revision": "e6fa5b239a016236c3a6",
+    "url": "/gotit/static/css/2.878c4f9a.chunk.css"
+  },
+  {
+    "revision": "d80a2c180fb06f03b2f6",
+    "url": "/gotit/static/css/main.0364647e.chunk.css"
+  },
+  {
+    "revision": "e6fa5b239a016236c3a6",
+    "url": "/gotit/static/js/2.cf8304c0.chunk.js"
+  },
+  {
+    "revision": "d80a2c180fb06f03b2f6",
+    "url": "/gotit/static/js/main.9f5f0c04.chunk.js"
+  },
+  {
+    "revision": "5d6f810820e923f22a16",
+    "url": "/gotit/static/js/runtime~main.10b2e90a.js"
+  }
+]);
