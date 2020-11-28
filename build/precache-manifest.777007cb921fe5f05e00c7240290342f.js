a9907cc84db766d7438250b65cb0588e1e5ba56c
jaybenaim <benaimjacob@gmail.com>
2020-11-27 21:53:20 -0500

    build

--- build/precache-manifest.777007cb921fe5f05e00c7240290342f.js
+++ build/precache-manifest.777007cb921fe5f05e00c7240290342f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1c6051484874495ecb425491018d62d",
+    "revision": "fb7f54b775319d89690f9a4c6c22b135",
     "url": "/gotit/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gotit/static/css/2.c9e7500d.chunk.css"
   },
   {
-    "revision": "17e3e154ac9a8c5f3f6b",
-    "url": "/gotit/static/css/main.b6a5549c.chunk.css"
+    "revision": "08e5fb766b56948665a3",
+    "url": "/gotit/static/css/main.e4a159dd.chunk.css"
   },
   {
     "revision": "ba71a7879f2a922d2b2b",
     "url": "/gotit/static/js/2.54b317e1.chunk.js"
   },
   {
-    "revision": "17e3e154ac9a8c5f3f6b",
+    "revision": "08e5fb766b56948665a3",
     "url": "/gotit/static/js/main.91a381b9.chunk.js"
   },
   {
