2934d03c74bbf1fba4460c02b04e33d1bc63dd41
jaybenaim <benaimjacob@gmail.com>
2020-11-29 18:24:55 -0500

    fixed jsconfig and basename

--- src/index.jsx
+++ src/index.jsx
@@ -30,7 +30,7 @@ const rrfProps = {
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-      <BrowserRouter basename="/gotit">
+      <BrowserRouter basename="gotit">
         <ErrorBoundary>
           <App />
         </ErrorBoundary>
