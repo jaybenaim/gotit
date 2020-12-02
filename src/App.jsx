f4cc2bb0b2ad39b69b2e28f5d1f69dea9ca5a9e7
jaybenaim <benaimjacob@gmail.com>
2020-12-01 21:56:53 -0500

    moved errors to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+﻿import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 import NavBar from "components/organisms/NavBar"
@@ -11,50 +11,67 @@ import AdminHome from "pages/Admin/AdminHome/AdminHome";
 import Posts from "components/molecules/Posts/list/Posts";
 import PostDetails from "components/molecules/Posts/details/Post-details";
 import { useEffect } from "react";
-import { messaging } from "./config/firebase"
-import errorReducers from "redux/reducers/errorReducers";
+
+import Notifications from "components/atoms/Notifications/Notifications";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  const setTokenToLocalStorage = (token) => {
-    return window.localStorage.setItem('token', token)
-  }
-  const getTokenFromLocalStorage = () => {
-    return window.localStorage.getItem('token')
-  }
+  // const setTokenToLocalStorage = (token) => {
+  //   return window.localStorage.setItem('token', token)
+  // }
+  // const getTokenFromLocalStorage = () => {
+  //   return window.localStorage.getItem('token')
+  // }
 
 
 
-  const messagingToken = () => {
-    // Get registration token. Initially this makes a network call, once retrieved
-    // subsequent calls to getToken will return from cache.
-    messaging.getToken({ vapidKey: process.env.REACT_APP_FIREBASE_MESSAGING_KEY })
-      .then((currentToken) => {
-        console.log(currentToken)
-        if (currentToken) {
-          setTokenToLocalStorage(currentToken);
-          // updateUIForPushEnabled(currentToken);
-        } else {
-          // Show permission request.
-          console.log('No registration token available. Request permission to generate one.');
-          // Show permission UI.
-          // updateUIForPushPermissionRequired();
-        }
-      }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // dispatchEvent(errorReducers("GET_ERRORS", "GET_ERRORS"))
-      });
+  // const messagingToken = () => {
+  //   // Get registration token. Initially this makes a network call, once retrieved
+  //   // subsequent calls to getToken will return from cache.
+  //   messaging.getToken({ vapidKey: process.env.REACT_APP_FIREBASE_MESSAGING_KEY })
+  //     .then((currentToken) => {
+  //       console.log(currentToken)
+  //       if (currentToken) {
+  //         setTokenToLocalStorage(currentToken);
+  //         // updateUIForPushEnabled(currentToken);
+  //       } else {
+  //         // Show permission request.
+  //         console.log('No registration token available. Request permission to generate one.');
+  //         // Show permission UI.
+  //         // updateUIForPushPermissionRequired();
+  //       }
+  //     }).catch((err) => {
+  //       console.log('An error occurred while retrieving token. ', err);
+  //       // dispatchEvent(errorReducers("GET_ERRORS", "GET_ERRORS"))
+  //     });
 
-  }
+  // }
+
+  // useEffect(() => {
+  //   messagingToken()
+  //   const token = getTokenFromLocalStorage()
+  //   console.log(token)
+  // })
+  const { errors } = useSelector((state) => state)
+  const [showErrors, setShowErrors] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("")
 
   useEffect(() => {
-    messagingToken()
-    const token = getTokenFromLocalStorage()
-    console.log(token)
-  })
+    if (Object.keys(errors).length > 0) {
+      setShowErrors(true)
+      setErrorMessage(errors.message)
+    }
+  }, [errors])
 
   return (
     <React.Fragment>
       <NavBar />
+      {showErrors && (
+        <Notifications
+          variant="danger"
+
+        />
+      )}
       <Switch>
         <PrivateRoute exact path="/admin">
           <AdminHome />
--- src/components/atoms/Notifications/Notifications.jsx
+++ src/components/atoms/Notifications/Notifications.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import { Toast, Alert, Button } from "react-bootstrap";
+import Heading from "components/atoms/Heading/Heading";
+import "./notifications.scss"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Notifications = ({
-  heading,
+  heading = "Error",
+  headingStyle = "h3",
   body,
   type,
   small,
@@ -11,36 +16,65 @@ const Notifications = ({
   handleConfirm,
   style,
 }) => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
+  // const { errors, notifications: { notification } } = useSelector((state) => state)
+  // const [errorMessage, setErrorMessage] = useState("")
+  const dispatch = useDispatch()
+
+
+  // useEffect(() => {
+  //   if (Object.keys(notification).length > 0) {
+  //     setShow(true)
+  //   }
+  // }, [notification])
+
+  // useEffect(() => {
+  //   if (Object.keys(errors).length > 0) {
+  //     setShow(true)
+  //     setErrorMessage(errors.message)
+  //   }
+  // }, [errors])
 
   return (
     show &&
     (type === "alert" ? (
       <Alert
+        className="notification"
         variant={variant}
         onClose={() => setShow(false)}
         style={style}
         dismissible
       >
         <Alert.Heading>{heading}</Alert.Heading>
+
         <p>{body}</p>
+
         {confirmButtonText && (
-          <Button variant="outline-danger" onClick={handleConfirm}>
+          <Button
+            variant="outline-danger"
+            onClick={handleConfirm}
+          >
             {confirmButtonText}{" "}
           </Button>
         )}
       </Alert>
     ) : (
-      type === "toast" && (
-        <Toast onClose={() => setShow(false)} style={style}>
-          <Toast.Header>
-            <strong className="mr-auto">{heading.toUpperCase()}</strong>
-            <small>{small}</small>
-          </Toast.Header>
-          <Toast.Body>{body}</Toast.Body>
-        </Toast>
-      )
-    ))
+        type === "toast" && (
+          <Toast
+            className="notification"
+            onClose={() => setShow(false)}
+            style={style}
+          >
+            <Toast.Header>
+              <Heading headingStyle={headingStyle} className="mr-auto">{heading}</Heading>
+              <small>{small}</small>
+            </Toast.Header>
+            <Toast.Body>
+              {body}
+            </Toast.Body>
+          </Toast>
+        )
+      ))
   );
 };
 
